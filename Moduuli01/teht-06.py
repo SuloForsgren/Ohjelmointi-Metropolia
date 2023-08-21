ab1d115ac1e8348ef468cda71ccad02006f54f7d
Sulo Forsgren <sulo.forsgren@gmail.com>
2023-08-21 14:18:07 +0300

    Loput moduulin tehtävät

---
+++ Moduuli01/teht-06.py
@@ -0,0 +1,14 @@
+import random
+
+luku = random.randint(0,10)
+luku2 = random.randint(0,10)
+luku3 = random.randint(0,10)
+
+print(luku,luku2,luku3)
+
+eka = random.randint(1,7)
+toka = random.randint(1,7)
+kolmas = random.randint(1,7)
+vika = random.randint(1,7)
+
+print(eka,toka,kolmas,vika)
