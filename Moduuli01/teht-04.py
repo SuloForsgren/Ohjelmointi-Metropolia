540295cef21d8ece86f09b3e4b651281117cdb1e
Sulo Forsgren <sulo.forsgren@gmail.com>
2023-08-21 11:44:24 +0300

    More files :)

---
+++ Moduuli01/teht-04.py
@@ -0,0 +1,11 @@
+luku1 = input("Luku1: ")
+luku2 = input("Luku2: ")
+luku3 = input("Luku3: ")
+
+luku1 = int(luku1)
+luku2 = int(luku2)
+luku3 = int(luku3)
+
+summa = luku1 + luku2 + luku3
+
+print(summa)
