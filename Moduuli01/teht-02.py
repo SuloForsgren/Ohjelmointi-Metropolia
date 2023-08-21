5f1c18247b1a3c5381097b57bfb073e5e75fbceb
Sulo Forsgren <sulo.forsgren@gmail.com>
2023-08-21 11:38:51 +0300

    Moduulin01 ekat tiedostot

---
+++ Moduuli01/teht-02.py
@@ -0,0 +1,4 @@
+ysade = input("Anna ympyrän säde: ")
+ysade = int(ysade)
+tulos = 3.14 * (ysade * ysade)
+print(f"Tulos: {tulos}")
---
+++ Moduuli01/teht-03.py
@@ -0,0 +1,8 @@
+kanta = input("Anna suorakulmion kanta: ")
+korkeus = input("Anna suorakulmion korkeus: ")
+
+kanta = int(kanta)
+korkeus = int(korkeus)
+
+piiri = (kanta * 2) + (korkeus * 2)
+print(piiri)
