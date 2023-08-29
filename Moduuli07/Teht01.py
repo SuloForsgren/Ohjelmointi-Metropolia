4b99d0dd6f53952b662a21d43d0f648988679549
Sulo Forsgren <sulo.forsgren@gmail.com>
2023-08-29 14:08:07 +0300

    moduuli07

---
+++ Moduuli07/Teht01.py
@@ -0,0 +1,4 @@
+kuukaudet = ("tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu", "heinäkuu", "elokuu", "syyskuu", "lokakuu", "marraskuu", "joulukuu")
+
+kk = int(input("Anna kuukausi: "))
+
