c19b4b2a4ac97a1b10f694cf4ba0219f764a6137
Sulo Forsgren <sulo.forsgren@gmail.com>
2023-09-11 15:59:55 +0300

    ICAO toimii

--- Moduuli07/Teht03.py
+++ Moduuli07/Teht03.py
@@ -1,23 +1,37 @@
 
 def lisaa_Lentokentta(Lentokentät) :
-    lkNimi = input("Anna lentokentän nimi: ")
-    icao = input("Anna lentokentän ICAO koodi: ")
-    Lentokentät[lkNimi] = icao
+    lkNimi = input("Anna lentoaseman nimi: ")
+    icao = input("Anna lentoaseman ICAO koodi: ")
+    Lentokentät[icao] = lkNimi
     return Lentokentät
 
-def hae_Lentokentta() :
-    return "Haettu"
-
+def hae_Lentokentta(Lentokentät) :
+    haetaan_ICAO_koodilla = input("Hae ICAO koodilla lentoasemaa: ")
+    for i in Lentokentät :
+        if haetaan_ICAO_koodilla in Lentokentät :
+            return Lentokentät[haetaan_ICAO_koodilla]
+        else :
+            return "Virheellinen ICAO Koodi!"
+        
 Lentokentät = {}
-
-syote = int(input("1, Syöttääksesi, 2 hakeaksesi ja 3 lopettaaksesi: "))
+print("Syötä numero -- 1, Syöttääksesi tietoja sanakirjaan")
+print("Syötä numero -- 2 hakeaksesi tietoja sanakirjasta ICAO koodilla")
+print("Syötä numero -- 3 lopettaaksesi ohjelman suorituksen")
+print()
+syote = int(input("Anna numero 1-3: "))
 
 while syote != 3 :
     
     if syote == 1 :
         print(lisaa_Lentokentta(Lentokentät))
-    elif syote == 2 :
-        print(hae_Lentokentta())
-    syote = int(input("1, Syöttääksesi, 2 hakeaksesi ja 3 lopettaaksesi: "))
 
-print("Lopetettiin, Heippa!")
+    elif syote == 2 :
+        print(hae_Lentokentta(Lentokentät))
+    
+    print("Syötä numero -- 1, Syöttääksesi tietoja sanakirjaan")
+    print("Syötä numero -- 2 hakeaksesi tietoja sanakirjasta ICAO koodilla")
+    print("Syötä numero -- 3 lopettaaksesi ohjelman suorituksen")
+    print()
+    syote = int(input("Anna numero 1-3: "))
+
+print("Lopetettiin, Heippa!")     
