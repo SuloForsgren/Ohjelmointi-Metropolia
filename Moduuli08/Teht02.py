1a5cd5ee9a4d130866e691f25c58c7652b6d95ee
Sulo Forsgren <sulo.forsgren@gmail.com>
2023-09-17 20:52:24 +0300

    Teht02

---
+++ Moduuli08/Teht02.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='Ruutt!',
+         autocommit=True
+         )
+
+def hae_maakoodilla_lentoasemat(maaKoodi):
+    sqlQuery = f"SELECT airport.type, count(airport.name) FROm airport WHERE iso_country = 'FI' GROUP BY airport.type;"
+    kursori = yhteys.cursor()
+    kursori.execute(sqlQuery)
+    tulos = kursori.fetchall()
+
+    return tulos
+
+maaKoodi = input("Syötä maakoodi: ")
+
+palautus = hae_maakoodilla_lentoasemat(maaKoodi)
+for i in palautus :
+    print(i[0], i[1])
+#print(f"ICAO-Koodilla {ICAO_koodi} löytyy lentokenttä {palautus[0][0]} ja sijainti on {palautus[0][1]}")
