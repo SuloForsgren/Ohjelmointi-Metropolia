cad172d781437cad7147fa00c6b3cb4c493e4f13
KoodiApina <sulonkoodit@gmail.com>
2023-10-23 20:21:22 +0300

    Moduuli09.. ;)

--- Moduuli09/Teht01.py
+++ Moduuli09/Teht01.py
@@ -1,1 +1,16 @@
-print("Hello")
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus, th_nopeus = 0, matka = 0) :
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.th_nopeus = th_nopeus
+        self.matka = matka
+
+auto = Auto("ABC-123", 142)
+
+print(
+f"""
+Rekkari: {auto.rekisteritunnus}
+Huippunopeus: {auto.huippunopeus}km/h
+Tämänhetkinen nopeus: {auto.th_nopeus}
+Auton kulkema matka: {auto.matka}
+""")
