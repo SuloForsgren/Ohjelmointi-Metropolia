ce26552baf8ebfaa53436334e17d1b5e056e1eff
KoodiApina <sulonkoodit@gmail.com>
2023-10-24 17:52:48 +0300

    Kilpailu ja M10

--- Moduuli09/teht04.py
+++ Moduuli09/teht04.py
@@ -1,9 +1,22 @@
+from random import randint
+
 class Kilpa_Autot():
-    def __init__(self):
-        pass
+    def __init__(self, rekisteritunnus, huippunopeus, nopeus = 0, matka = 0):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus    
+        self.nopeus = nopeus
+        self.matka = matka
+        
+    def kiihdytä(self, muutos):
+        print(muutos)
 
 
+autot = []
+for luku in range(1,11) :
+    hnopeus = randint(100,200)
+    auto = Kilpa_Autot(f"ABC-{luku}", hnopeus)
+    autot.append(auto)
 
 
-
-
+for i in autot :
+    auto.kiihdytä(randint(-10,15))
